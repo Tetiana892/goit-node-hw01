2ff3aa2c4b16ec543d31dbbe9878ac7d8009221d
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-07-26 16:23:23 +0300

    update index.js

--- index.js
+++ index.js
@@ -19,25 +19,21 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const allContacts = await contacts.listContacts();
       return console.table(allContacts);
-      break;
 
     case "get":
       // ... id
       const oneContact = await contacts.getContactById(id);
       return console.log(oneContact);
-      break;
 
     case "add":
       // ... name email phone
       const newContact = await contacts.addContact({ name, email, phone });
       return console.log(newContact);
-      break;
 
     case "remove":
       // ... id
       const deleteContact = await contacts.removeContact(id);
       return console.log(deleteContact);
-      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
