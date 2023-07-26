341b4a2d646db0fded7f51d7f25c178af9145309
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-07-26 14:57:46 +0300

    summary files

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cкріншоти результату виконання кожної команди
+
+### 1) Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+https://monosnap.com/file/KiAbLx94tjbhTcCsoqqfTpbT9FdzPA
+![command list] (./assets/screenshot_1.jpg)
+
+### 2) Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+https://monosnap.com/file/9tmDymL5BwZ6UHMd924JlpdjYcyAw8
+![command get] (./assets/screenshot_2.jpg)
+
+### 3) Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/SzfTicZTUKE2Nx9YzxiUsDEbFwiP2Q
+![command add] (./assets/screenshot_3.jpg)
+
+### 4) Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/x8vws4O1LxMqtgazyDVLdXedc7cDcO
+![command remove] (./assets/screenshot_4.jpg)
---
+++ contacts.js
@@ -0,0 +1,54 @@
+// contacts.js
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+/*
+ * Раскомментируй и запиши значение
+const contactsPath = ;
+ */
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  return result || null;
+}
+console.log(getContactById);
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+console.log(removeContact);
+
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+console.log(addContact);
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
